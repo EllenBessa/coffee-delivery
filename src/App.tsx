d4e4ec905bc65f0c39f5c98f718f3e66eedbe504
EllenBessa <ellenbessa96@gmail.com>
2023-07-06 13:58:35 -0300

    feat: :lipstick: Styled Components - Adicionando global, themes e @types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>Hello Word</h1>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
 }
