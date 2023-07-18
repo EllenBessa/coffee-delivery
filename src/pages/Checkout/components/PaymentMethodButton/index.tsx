858ba19668c4958f6f8ac4ca89ccfd363760aef3
EllenBessa <ellenbessa96@gmail.com>
2023-07-18 19:35:05 -0300

    feat: :lipstick: Componente PaymentMethodButton da página Checkout

---
+++ src/pages/Checkout/components/PaymentMethodButton/index.tsx
@@ -0,0 +1,43 @@
+import { ReactElement } from "react";
+
+import { Bank, CreditCard, Money } from "@phosphor-icons/react";
+
+type PaymentMethod = "credit" | "debit" | "money";
+
+interface ButtonProps {
+  icon: ReactElement;
+  text: string;
+}
+
+interface PaymentMethodsButtonProps {
+  paymentMethod: PaymentMethod;
+  onSelectPaymentMethod?: (paymentMethod: PaymentMethod) => void;
+}
+
+const buttonVariant: Record<PaymentMethod, ButtonProps> = {
+  credit: {
+    icon: <CreditCard size={16} />,
+    text: "Cartão de crédito"
+  },
+
+  debit: {
+    icon: <Bank size={16} />,
+    text: "Cartão de débito"
+  },
+
+  money: {
+    icon: <Money size={16} />,
+    text: "Dinheiro"
+  }
+};
+
+export function PaymentMethodButton({
+  paymentMethod
+}: PaymentMethodsButtonProps) {
+  return (
+    <button type="button">
+      {buttonVariant[paymentMethod].icon}
+      {buttonVariant[paymentMethod].text}
+    </button>
+  );
+}
